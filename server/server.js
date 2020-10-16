37339a81a503d9479a499671086c72199c485ec2
Jake Stucky <jakestucky@gmnail.com>
2020-10-16 12:01:24 -0500

    troubleshooting the DB query

--- server/server.js
+++ server/server.js
@@ -26,57 +26,61 @@ app.get('/calculator', (req, res) => {
 });
 
 app.post('/calculator', (req, res) => {
-  console.log('I got a request!', req.body);
-  let newMathObject = req.body;
+  // console.log('I got a request!', req.body);
+  // let newMathObject = req.body;
   // Server-side validation
-  if (newMathObject.mathOperator === undefined) {
-    res.sendStatus(400);
-    alert('math operator must be defined');
-    return;
-  }
-  serverSideCalc = (newMathObject) => {
-    //if data-oper = logic
-    if (newMathObject.mathOperator === '-') {
-      calcSolution = newMathObject.firstNumber - newMathObject.secondNumber;
-      console.log('answer is:', calcSolution);
-    } else if (newMathObject.mathOperator === '+') {
-      calcSolution =
-        Number(newMathObject.firstNumber) + Number(newMathObject.secondNumber);
-      console.log('answer is:', calcSolution);
-    } else if (newMathObject.mathOperator === '/') {
-      calcSolution = newMathObject.firstNumber / newMathObject.secondNumber;
-      console.log('answer is:', calcSolution);
-    } else if (newMathObject.mathOperator === '*') {
-      calcSolution = newMathObject.firstNumber * newMathObject.secondNumber;
-      console.log('answer is:', calcSolution);
-    } else {
-      console.log('this aint right');
-    }
-  }; //db call to add to DB
+  // if (newMathObject.mathOperator === undefined) {
+  //   res.sendStatus(400);
+  //   alert('math operator must be defined');
+  //   return;
+  // }
   const queryText = `INSERT INTO "calc_history" 
-    ("firstNumber", "mathOperator", "secondNumber","solution")
+    ("firstNumber", "mathOperator", "secondNumber")
     VALUES ($1, $2, $3, $4);`;
   pool
     .query(queryText, [
       req.body.firstNumber,
       req.body.mathOperator,
       req.body.secondNumber,
-      newMathObject.solution,
     ])
+
+    .then(() => {
+      console.log('we are sending:', pool.query);
+      res.sendStatus(201);
+    })
     .catch((err) => {
       console.error('Error completing child info post query', err);
       res.sendStatus(500);
     });
-  res.sendStatus(201);
+  // serverSideCalc = (newMathObject) => {
+  //   //if data-oper = logic
+  //   if (newMathObject.mathOperator === '-') {
+  //     calcSolution = newMathObject.firstNumber - newMathObject.secondNumber;
+  //     console.log('answer is:', calcSolution);
+  //   } else if (newMathObject.mathOperator === '+') {
+  //     calcSolution =
+  //       Number(newMathObject.firstNumber) + Number(newMathObject.secondNumber);
+  //     console.log('answer is:', calcSolution);
+  //   } else if (newMathObject.mathOperator === '/') {
+  //     calcSolution = newMathObject.firstNumber / newMathObject.secondNumber;
+  //     console.log('answer is:', calcSolution);
+  //   } else if (newMathObject.mathOperator === '*') {
+  //     calcSolution = newMathObject.firstNumber * newMathObject.secondNumber;
+  //     console.log('answer is:', calcSolution);
+  //   } else {
+  //     console.log('this aint right');
+  //   }
+  // }; //db call to add to DB
+
+  // res.sendStatus(201);
 
   // add new key to object
-  newMathObject.solution = calcSolution;
-  serverSideCalcArray.push(newMathObject);
+  // newMathObject.solution = calcSolution;
+  // serverSideCalcArray.push(newMathObject);
 
-  console.log('new obj', newMathObject);
+  // console.log('new obj', newMathObject);
 });
 app.delete('/calculator', (req, res) => {
   serverSideCalcArray = [];
   res.sendStatus(201);
 });
-module.exports = calcSolution;
