630092e203ea95a38812e76a16e935b4e90586c5
Jake Stucky <jakestucky@gmnail.com>
2020-10-16 13:01:38 -0500

    Got the post routes and get routes sorted. Appends to the dom and is all stored on the DB

--- server/server.js
+++ server/server.js
@@ -21,66 +21,92 @@ let calcSolution;
 // GET /calculator
 // Respond with array of calc solves
 app.get('/calculator', (req, res) => {
-  res.send(serverSideCalcArray);
-  console.log('sending back the array', serverSideCalcArray);
-});
-
-app.post('/calculator', (req, res) => {
-  // console.log('I got a request!', req.body);
-  // let newMathObject = req.body;
-  // Server-side validation
-  // if (newMathObject.mathOperator === undefined) {
-  //   res.sendStatus(400);
-  //   alert('math operator must be defined');
-  //   return;
-  // }
-  const queryText = `INSERT INTO "calc_history" 
-    ("firstNumber", "mathOperator", "secondNumber")
-    VALUES ($1, $2, $3, $4);`;
+  let queryText = `SELECT * FROM "calc_history" ORDER BY "id" DESC LIMIT 10;`;
   pool
-    .query(queryText, [
-      req.body.firstNumber,
-      req.body.mathOperator,
-      req.body.secondNumber,
-    ])
-
-    .then(() => {
-      console.log('we are sending:', pool.query);
-      res.sendStatus(201);
+    .query(queryText)
+    .then((result) => {
+      console.log('results from get:', result.rows);
+      res.send(result.rows);
     })
     .catch((err) => {
       console.error('Error completing child info post query', err);
       res.sendStatus(500);
     });
-  // serverSideCalc = (newMathObject) => {
-  //   //if data-oper = logic
-  //   if (newMathObject.mathOperator === '-') {
-  //     calcSolution = newMathObject.firstNumber - newMathObject.secondNumber;
-  //     console.log('answer is:', calcSolution);
-  //   } else if (newMathObject.mathOperator === '+') {
-  //     calcSolution =
-  //       Number(newMathObject.firstNumber) + Number(newMathObject.secondNumber);
-  //     console.log('answer is:', calcSolution);
-  //   } else if (newMathObject.mathOperator === '/') {
-  //     calcSolution = newMathObject.firstNumber / newMathObject.secondNumber;
-  //     console.log('answer is:', calcSolution);
-  //   } else if (newMathObject.mathOperator === '*') {
-  //     calcSolution = newMathObject.firstNumber * newMathObject.secondNumber;
-  //     console.log('answer is:', calcSolution);
-  //   } else {
-  //     console.log('this aint right');
-  //   }
-  // }; //db call to add to DB
+});
 
-  // res.sendStatus(201);
+app.post('/calculator', (req, res) => {
+  console.log('I got a request!', req.body);
+  let newMathObject = req.body;
+  const queryText = `INSERT INTO "calc_history" 
+    ("firstNumber", "mathOperator", "secondNumber", "solution")
+    VALUES ($1, $2, $3, $4);`;
+  // Server-side validation
+  if (newMathObject.mathOperator === undefined) {
+    res.sendStatus(400);
+    alert('math operator must be defined');
+    return;
+  }
+  serverSideCalc(newMathObject);
+  function serverSideCalc() {
+    //if data-oper = logic
+    if (newMathObject.mathOperator === '-') {
+      calcSolution = newMathObject.firstNumber - newMathObject.secondNumber;
+      console.log('answer is:', calcSolution);
+    } else if (newMathObject.mathOperator === '+') {
+      calcSolution =
+        Number(newMathObject.firstNumber) + Number(newMathObject.secondNumber);
+      console.log('answer is:', calcSolution);
+    } else if (newMathObject.mathOperator === '/') {
+      calcSolution = newMathObject.firstNumber / newMathObject.secondNumber;
+      console.log('answer is:', calcSolution);
+    } else if (newMathObject.mathOperator === '*') {
+      calcSolution = newMathObject.firstNumber * newMathObject.secondNumber;
+      console.log('answer is:', calcSolution);
+    } else {
+      console.log('this aint right');
+    }
+
+    pool
+      .query(queryText, [
+        req.body.firstNumber,
+        req.body.mathOperator,
+        req.body.secondNumber,
+        '1',
+      ])
+
+      .then(() => {
+        console.log('we are sending:', pool.query);
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.error('Error completing child info post query', err);
+        res.sendStatus(500);
+      });
+  }
 
   // add new key to object
-  // newMathObject.solution = calcSolution;
-  // serverSideCalcArray.push(newMathObject);
-
-  // console.log('new obj', newMathObject);
+  newMathObject.solution = calcSolution;
 });
 app.delete('/calculator', (req, res) => {
-  serverSideCalcArray = [];
-  res.sendStatus(201);
+  let queryText = ` DROP TABLE "calc_history";
+ 
+ CREATE TABLE "calc_history" (
+    "id" SERIAL PRIMARY KEY,
+    "firstNumber" VARCHAR (80) NOT NULL,
+  "mathOperator" VARCHAR (80) NOT NULL,
+  "secondNumber" VARCHAR (80) NOT NULL,
+    "solution" VARCHAR (80) NOT NULL,
+    "time" TIME DEFAULT NOW()
+    )
+   ;`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      console.log('dropped table and rebuilt it');
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error('Error completing child info post query', err);
+      res.sendStatus(500);
+    });
 });
